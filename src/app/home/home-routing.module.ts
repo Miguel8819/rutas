1d8ee9d9f5b9f84c37564752dca40a8e91e93e79
angular <migueldesoftware@gmail.com>
2024-12-24 17:25:09 -0300

    rutas profile

---
+++ src/app/home/home-routing.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home.component';
+
+const routes: Routes = [
+  {
+    path: 'Home',
+    component: HomeComponent,
+  },
+
+  {
+    path: 'profile',
+    loadChildren: () => 
+      import('./pages/profile/profile.module').then((m) =>m.ProfileModule),    
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class HomeRoutingModule { }
